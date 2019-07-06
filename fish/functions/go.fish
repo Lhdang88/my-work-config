d149ef5e1f473953fa94a2ecf743cae17e4ca412
Hai Dang Le <haidangle@Dangs-Mac-Mini.local>
2019-07-07 01:31:41 +0200

    fix: setting go path correctly with fish

--- fish/functions/go.fish
+++ fish/functions/go.fish
@@ -1,12 +1,12 @@
 function gohere -d "Sets up a Go Workspace in the specified Dir"
     mkdir -p $argv/bin $argv/src $argv/pkg;
-    set -g GOPATH (pwd)/$argv;
-    set -g PATH $PATH (pwd)/$argv/bin;
+    set -x GOPATH (pwd)/$argv;
+    set -x PATH $PATH (pwd)/$argv/bin;
     echo New GOPATH is $GOPATH;
 end
 
 function goset -d "Sets up the Go environment in current Dir"
-    set -g GOPATH (pwd);
-    set -g PATH $PATH (pwd)/bin;
+    set -x GOPATH (pwd);
+    set -x PATH $PATH (pwd)/bin;
     echo New GOPATH is $GOPATH;
 end
