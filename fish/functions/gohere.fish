fac8188802030c234e4afd12b23287f998547cae
Hai Dang Le <haidangle@Dangs-Mac-Mini.local>
2019-07-07 01:07:44 +0200

    feat: add goset func

--- fish/functions/gohere.fish
+++
@@ -1,6 +0,0 @@
-function gohere -d "Sets up a Go Workspace in the specified Dir"
-    mkdir -p $argv/bin $argv/src $argv/pkg;
-    set -x GOPATH (pwd)/$argv;
-    set -x PATH $PATH (pwd)/$argv/bin;
-    echo New GOPATH is $GOPATH;
-end
